93d5d3bbf52b8cd729e7860da02eafccb9a3efec
Alan Eicker <Alan.Eicker@allstate.com>
2019-05-29 13:15:51 -0500

    updates migrations

--- server/migration.js
+++ server/migration.js
@@ -1,57 +1,40 @@
 module.exports = {
   up: `
     CREATE TABLE users (
-      id INTEGER PRIMARY KEY, 
-      name VARCHAR,
-      email VARCHAR,
-      username VARCHAR, 
-      password VARCHAR
+      id INT NOT NULL AUTO_INCREMENT, 
+      username VARCHAR(15) NOT NULL, 
+      password VARCHAR(15) NOT NULL,
+      PRIMARY KEY (id)
     );
     
     INSERT INTO users (
-      name,
-      email,
       username, 
       password
     ) VALUES (
-      'Alan Eicker',
-      'alaneicker@gmail.com',
       'alaneicker',
       'bcrypt$2b$14$.5OgqP0JeLVbd/6qQ6O4S.SQyTjQkG/LSBFH3MjLR6G8mnGo1vMFG'
     ),
     (
-      'John Doe',
-      'jdoe@gmail.com',
       'jdoe',
       'bcrypt$2b$14$.5OgqP0JeLVbd/6qQ6O4S.SQyTjQkG/LSBFH3MjLR6G8mnGo1vMFG'
     ),
     (
-      'Fred Nelson',
-      'fnelson@yahoo.com',
       'frednelson',
       'bcrypt$2b$14$.5OgqP0JeLVbd/6qQ6O4S.SQyTjQkG/LSBFH3MjLR6G8mnGo1vMFG'
     ),
     (
-      'Mary Smith',
-      'mary.smith@gmail.com',
       'msmith',
       'bcrypt$2b$14$.5OgqP0JeLVbd/6qQ6O4S.SQyTjQkG/LSBFH3MjLR6G8mnGo1vMFG'
     ),
     (
-      'Bob Newhart',
-      'bob.newhart@gmail.com',
       'bobnewhart',
       'bcrypt$2b$14$.5OgqP0JeLVbd/6qQ6O4S.SQyTjQkG/LSBFH3MjLR6G8mnGo1vMFG'
     ),
     (
-      'Paul Monson',
-      'p.mpnson@gmail.com',
       'paulmonson',
       'bcrypt$2b$14$.5OgqP0JeLVbd/6qQ6O4S.SQyTjQkG/LSBFH3MjLR6G8mnGo1vMFG'
     ),
     (
-      'John Jacoby',
-      'john.jacoby@yahoo.com',
       'johnj',
       'bcrypt$2b$14$.5OgqP0JeLVbd/6qQ6O4S.SQyTjQkG/LSBFH3MjLR6G8mnGo1vMFG'
     );
