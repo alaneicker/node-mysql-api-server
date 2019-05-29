15ae7233e80e1cfa9c34c00b6c6a029eb3bf3637
Alan Eicker <Alan.Eicker@allstate.com>
2019-05-29 14:45:54 -0500

    Creates custom db migration

--- server/migration.js
+++ server/migration.js
@@ -1,4 +1,58 @@
 const connection = require('./db-connection');
-const migration = require('mysql-migrations');
 
-migration.init(connection, __dirname + '/migrations');
+const up = connection.query(`
+  CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT, 
+    username VARCHAR(15) NOT NULL, 
+    password VARCHAR(15) NOT NULL,
+    PRIMARY KEY (id)
+  );
+
+  INSERT INTO users (
+    username, 
+    password
+  ) VALUES (
+    'alaneicker',
+    'bcrypt$2b$14$.5OgqP0JeLVbd/6qQ6O4S.SQyTjQkG/LSBFH3MjLR6G8mnGo1vMFG'
+  ),
+  (
+    'jdoe',
+    'bcrypt$2b$14$.5OgqP0JeLVbd/6qQ6O4S.SQyTjQkG/LSBFH3MjLR6G8mnGo1vMFG'
+  ),
+  (
+    'frednelson',
+    'bcrypt$2b$14$.5OgqP0JeLVbd/6qQ6O4S.SQyTjQkG/LSBFH3MjLR6G8mnGo1vMFG'
+  ),
+  (
+    'msmith',
+    'bcrypt$2b$14$.5OgqP0JeLVbd/6qQ6O4S.SQyTjQkG/LSBFH3MjLR6G8mnGo1vMFG'
+  ),
+  (
+    'bobnewhart',
+    'bcrypt$2b$14$.5OgqP0JeLVbd/6qQ6O4S.SQyTjQkG/LSBFH3MjLR6G8mnGo1vMFG'
+  ),
+  (
+    'paulmonson',
+    'bcrypt$2b$14$.5OgqP0JeLVbd/6qQ6O4S.SQyTjQkG/LSBFH3MjLR6G8mnGo1vMFG'
+  ),
+  (
+    'johnj',
+    'bcrypt$2b$14$.5OgqP0JeLVbd/6qQ6O4S.SQyTjQkG/LSBFH3MjLR6G8mnGo1vMFG'
+  );
+`, (error, results) => {
+  if (error) {
+    throw new Error(error);
+  };
+  
+  console.log('DATABASE MIGRATED: ', results);
+});
+
+const down = connection.query('DROP TABLE users', (error, results) => {
+  if (error) {
+    throw new Error(error);
+  };
+
+  console.log('DATABASE TABLE DROPPED: ', results);
+});
+
+module.exports = { up, down };
