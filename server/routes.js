6fd63e26a51c1e3d12c1b6e32265065a71e6a692
Alan Eicker <alaneicker@gmail.com>
2019-07-27 11:08:06 -0500

    Refactors route promises to use async/await

--- server/routes.js
+++ server/routes.js
@@ -8,44 +8,49 @@ const {
   deleteRecord,
 } = require('./queries');
 
-routes.get('/api/get-all-records', (req, res) => {
-  getAllRecords(req.body)
-    .then(resData => {
-      res.send(resData);
-    })
-    .catch(err => console.log(err));
+routes.get('/api/get-all-records', async (req, res) => {
+  try {
+    const response = await getAllRecords(req.body);
+    res.send(response);
+  } catch (err) {
+    throw new Error(err);
+  }
 });
 
-routes.get('/api/get-record/:id', (req, res) => {
-  getRecordById(req.body, req.params.id)
-    .then(resData => {
-      res.send(resData);
-    })
-    .catch(err => console.log(err));
+routes.get('/api/get-record/:id', async (req, res) => {
+  try {
+    const response = await getRecordById(req.body, req.params.id);
+    res.send(response);
+  } catch (err) {
+    throw new Error(err);
+  }
 });
 
-routes.post('/api/add-record', (req, res) => {
-  addRecord(req.body)
-    .then(resData => {
-      res.send(resData);
-    })
-    .catch(err => console.log(err));
+routes.post('/api/add-record', async (req, res) => {
+  try {
+    const response = await addRecord(req.body);
+    res.send(response);
+  } catch (err) {
+    throw new Error(err);
+  }
 });
 
-routes.put('/api/update-record/:id', (req, res) => {
-  updateRecord(req.body, req.params.id)
-    .then(resData => {
-      res.send(resData);
-    })
-    .catch(err => console.log(err));
+routes.put('/api/update-record/:id', async (req, res) => {
+  try {
+    const response = await updateRecord(req.body, req.params.id);
+    res.send(response);
+  } catch (err) {
+    throw new Error(err);
+  }
 });
 
-routes.delete('/api/delete-record/:id', (req, res) => {
-  deleteRecord(req.body, req.params.id)
-    .then(resData => {
-      res.send(resData);
-    })
-    .catch(err => console.log(err));
+routes.delete('/api/delete-record/:id', async (req, res) => {
+  try {
+    const response = await deleteRecord(req.body, req.params.id);
+    res.send(response);
+  } catch (err) {
+    throw new Error(err);
+  }
 });
 
 module.exports = routes;
